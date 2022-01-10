304e51869064aeea556c9f53e13c3813628d89a8
Castro0o <andre@andrecastro>
2022-01-10 16:33:54 +0100

    starting to map movies_metadata_json

--- run_mapping.sh
+++ run_mapping.sh
@@ -2,6 +2,11 @@
 
 # convert mapping from yaml to ttl
 docker run --rm -it -v $(pwd)/data:/data rmlio/yarrrml-parser:latest  -i /data/movie_map.yarrr.yml -o /data/tmp/movie_map.yarrr.ttl
-
 # map
 docker run --rm -v $(pwd)/data/:/data/ rmlio/rmlmapper-java -m /data/tmp/movie_map.yarrr.ttl -s turtle -o /data/output/all.ttl
+
+# # convert mapping from yaml to ttl
+# docker run --rm -it -v $(pwd)/data:/data rmlio/yarrrml-parser:latest  -i /data/test_map.yarrr.yml -o /data/tmp/test_map.yarrr.ttl
+
+# # map
+# docker run --rm -v $(pwd)/data/:/data/ rmlio/rmlmapper-java -m /data/tmp/test_map.yarrr.ttl -s turtle -o /data/output/test.ttl
