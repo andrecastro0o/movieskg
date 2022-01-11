0c36432362d35b38d391172e4d8086c799f13008
Castro0o <andre@andrecastro>
2022-01-11 17:15:28 +0100

    docker pull rmlio images in run_mappings.sh

--- run_mapping.sh
+++ run_mapping.sh
@@ -3,6 +3,10 @@
 # preprocess 1000_keywords.csv to data/fixed_1000_keywords.csv
 python3 scripts/fix_csv.py
 
+# pull docker images
+docker pull rmlio/yarrrml-parser:latest
+docker pull rmlio/rmlmapper-java
+
 # convert mapping from yaml to ttl
 docker run --rm -it -v $(pwd)/data:/data rmlio/yarrrml-parser:latest  -i /data/movie_map.yarrr.yml -o /data/tmp/movie_map.yarrr.ttl
 # map
