ceb6c5d458f7cfe65ecfd394149abff4849af8d6
Castro0o <andre@andrecastro>
2022-01-11 09:55:42 +0100

    director as number; writer as number; writer bug

--- data/explore_data_json.py
+++ data/explore_data_json.py
@@ -18,3 +18,13 @@ for i, entry in enumerate(json_content):
             all_parent_keys.append(k)
 
 print(all_parent_keys)
+
+
+for i, entry in enumerate(json_content):
+    # if i == 0:
+    #     print(type(entry['id']))
+    #     print(type(entry))
+    if entry['id'] == 102:
+        for crew_member in entry['crew']:
+            if crew_member['job'] == 'Director':
+                print(crew_member)
